5744ef6b898220cd24b5662ca80ed174bc42a83d
dondonee <dyn.hwang@gmail.com>
2023-01-19 19:10:24 +0900

    feat: Luhn's Algorithm의 곱하기 단계 기능 추가

--- pset1/credit.c
+++ pset1/credit.c
@@ -11,10 +11,6 @@
         뒤에서 두번째 자리부터, 한 자리씩 건너 뛰며 2를 곱한 뒤, 저장한다.
     더하기 연산 - 0번쨰 요소부터 하나씩 가산하며 각 자릿수의 총 합을 구한다.
         10 이상인 경우 1을 더하고 뒷자리를 더한다.
-
-
-
-    
 */
 int main(void)
 {
@@ -33,16 +29,36 @@ int main(void)
 
     //카드번호 배열에 한자리씩 저장하기
     int numbers[count];
-    count --;
-    for (int i = 0; i <= count; i++)
+    int exponent = count - 1;
+    nTemp = n;
+    for (int i = 0; i < count; i++)
     {
-        int digit = (int)pow(10.0, (double)count - i);
-        numbers[i] = (int)(n / digit);
-        n %= digit;
+        int digit = (int)pow(10.0, (double)exponent - i);
+        numbers[i] = (int)(nTemp / digit);
+        nTemp %= digit;
     }
 
-    //$$ 카드변호 배열 출력
-    for (int i = 0; i <= count; i++)
+    //$$test: 카드변호 배열 출력
+    for (int i = 0; i < count; i++)
+    {
+        printf("%i\n", numbers[i]);
+    }
+
+    //카드번호 자릿수의 홀짝 판단. 총 자릿수가 짝수 -> 배열[짝수]일때 곱하기 2
+    int check = count % 2;
+
+    //대상 digit에 곱하기 2
+    for (int i = 0; i < count; i++)
+    {
+        if (i % 2 == check)
+        {
+            numbers[i] *= 2;
+        }
+    }
+
+    //$$test: 카드변호 배열 출력
+    printf("---\n");
+    for (int i = 0; i < count; i++)
     {
         printf("%i\n", numbers[i]);
     }
