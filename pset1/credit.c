0734c4978c6383c9469b787e3c094bddd043be4e
dondonee <dyn.hwang@gmail.com>
2023-01-19 23:14:14 +0900

    style: 변수명 변경(result -> sum)

--- pset1/credit.c
+++ pset1/credit.c
@@ -11,11 +11,11 @@
         - 카드번호의 자릿수가 짝수인지 홀수인지 판단한다.
         - 짝수인 경우 배열의 요소가 짝수 번째일 때 2를 곱해준다. (홀수인 경우에는 홀수 번째에 곱해준다.)
     6.더하기 연산
-        - 배열의 0번째 요소부터 0으로 초기화한 result 변수에 가산하며 계산한다.
-        - 요소가 10 이상인 경우 십의 자리와 일의 자리를 한 자리의 digit으로 분리하여 result 변수에 하나씩 더한다.
+        - 배열의 0번째 요소부터 0으로 초기화한 sum 변수에 가산하며 계산한다.
+        - 요소가 10 이상인 경우 십의 자리와 일의 자리를 한 자리의 digit으로 분리하여 sum 변수에 하나씩 더한다.
     7.카드의 종류를 판단한다.
-        - result % 10 != 0인 경우 INVALID
-        - result % 10 == 0인 경우 카드사의 종류를 판단한다.
+        - sum % 10 != 0인 경우 INVALID
+        - sum % 10 == 0인 경우 카드사의 종류를 판단한다.
     8.결과값 출력
 */
 int main(void)
@@ -55,19 +55,20 @@ int main(void)
         }
     }
 
-    int result = 0;
+    // 각 digit 더하기
+    int sum = 0;
     for (int i = 0; i < count; i++)
     {
         int element = numbers[i];
 
         if (element > 9)
         {
-            result += element / 10;
-            result += element % 10;
+            sum += element / 10;
+            sum += element % 10;
         }
         else
         {
-            result += element;
+            sum += element;
         }
     }
 }
