74de98d9ab6c3c8045f098f00db05325cb8a1d4b
dondonee <dyn.hwang@gmail.com>
2023-01-24 17:27:46 +0900

    feat: sentences 카운트 코드 추가

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -5,14 +5,15 @@
 
 int main(void)
 {
-    //Getting User Input
+    // Getting User Input
     string sText = get_string("Text: ");
     int nLength = strlen(sText);
 
-    //Letters
+    // Letters
     int nLetterCnt = 0;
     int nWordCnt = 0;
-    for(int i = 0; i < nLength; i++)
+    int nSentenceCnt = 0;
+    for (int i = 0; i < nLength; i++)
     {
         char cLetter = sText[i];
 
@@ -21,13 +22,26 @@ int main(void)
             nLetterCnt++;
         };
 
-        if (cLetter == ' ' || (cLetter == '.' && i == nLength -1))
+        if (cLetter == ' ' || (cLetter == '.' && i == nLength - 1))
         {
             nWordCnt++;
+        }
+
+        if (cLetter == '.')
+        {
+            char str1[2];
+            char str2[3];
+            strncpy(str1, &sText[i - 2], 2);
+            strncpy(str2, &sText[i - 3], 3);
+
+            if (strncmp(str1, "Mr", 2) != 0 && strncmp(str2, "Mrs", 3) != 0)
+            {
+                nSentenceCnt++;
+            }
         }
     }
 
     printf("%i letter(s)\n", nLetterCnt);
     printf("%i word(s)\n", nWordCnt);
-
+    printf("%i sentence(s)\n", nSentenceCnt);
 }
