52412f1168030504e61f90b042cd2ab3e59670d5
dondonee <dyn.hwang@gmail.com>
2023-01-24 21:32:55 +0900

    refactor: [Readability] 코드 추상화
    카운트 함수 추가 : countLetter, countWord, countSentence
    인덱스 계산 함수 추가 : calculateIndex

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -4,51 +4,24 @@
 #include <string.h>
 #include <math.h>
 
+float countLetter(string, int);
+float countWord(string, int);
+float countSentence(string, int);
+int calculateIndex (float letter, float word, float sentence);
+
 int main(void)
 {
     // Getting User Input
-    string sText = get_string("Text: ");
-    int nLength = strlen(sText);
+    string text = get_string("Text: ");
+    int textLen = strlen(text);
 
     // Count
-    int nLetterCnt = 0;
-    int nWordCnt = 1;
-    int nSentenceCnt = 0;
-    for (int i = 0; i < nLength; i++)
-    {
-        char cLetter = sText[i];
-
-        // Letters
-        if (isalpha(cLetter))
-        {
-            nLetterCnt++;
-        };
-
-        // Words
-        if (cLetter == ' ' && isalpha(sText[i + 1]))
-        {
-            nWordCnt++;
-        }
-
-        // Sentences
-        if (cLetter == '.' || cLetter == '!' || cLetter == '?' || cLetter == ':')
-        {
-            char str1[2];
-            char str2[3];
-            strncpy(str1, &sText[i - 2], 2);
-            strncpy(str2, &sText[i - 3], 3);
-
-            if (strncmp(str1, "Mr", 2) != 0 && strncmp(str2, "Mrs", 3) != 0)
-            {
-                nSentenceCnt++;
-            }
-        }
-    }
+    float letterCnt = countLetter(text, textLen);
+    float wordCnt = countWord(text, textLen);
+    int sentenceCnt = countSentence(text, textLen);
 
     // Coleman-Liau Index
-    float L = ((float)nLetterCnt / (float)nWordCnt) * 100.00;
-    float S = ((float)nSentenceCnt / (float)nWordCnt) * 100.00;
-    int nGrade = round(0.0588 * L - 0.296 * S - 15.8);
+    int nGrade = calculateIndex(letterCnt, wordCnt, sentenceCnt);
 
     // Print the result
     if (nGrade < 1)
@@ -63,4 +36,71 @@ int main(void)
     {
         printf("Grade %i\n", nGrade);
     }
+}
+
+float countLetter(string text, int length)
+{
+    int result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        char c = text[i];
+
+        if (isalpha(c))
+        {
+            result++;
+        };
+    }
+
+    return result;
+}
+
+float countWord(string text, int length)
+{
+    int result = 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        char c = text[i];
+
+        if (c == ' ' && isalpha(text[i + 1]))
+        {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+float countSentence(string text, int length)
+{
+    int result = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        char c = text[i];
+
+        if (c == '.' || c == '!' || c == '?' || c == ':')
+        {
+            char str1[2];
+            char str2[3];
+            strncpy(str1, &text[i - 2], 2);
+            strncpy(str2, &text[i - 3], 3);
+
+            if (strncmp(str1, "Mr", 2) != 0 && strncmp(str2, "Mrs", 3) != 0)
+            {
+                result++;
+            }
+        }
+    }
+
+    return result;
+}
+
+int calculateIndex (float letter, float word, float sentence)
+{
+    float L = letter / word * 100.00;
+    float S = sentence / word * 100.00;
+    int result = round(0.0588 * L - 0.296 * S - 15.8);
+
+    return result;
 }
