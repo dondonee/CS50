2147650483e07455eca82a4ac8565e45c840dcf4
dondonee <dyn.hwang@gmail.com>
2023-02-01 12:32:44 +0900

    feat: [Substitution 생성] 명령행 인자 값 유효여부 검사 기능 추가
    - checkArg() 추가

---
+++ pset2/substitution/substitution.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool checkArg(int, char **);
+
+int main(int argc, char **argv)
+{
+    if (checkArg(argc, argv) == false)
+    {
+        printf("usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        printf("Succces.\n");
+        return 0;
+    }
+}
+
+bool checkArg(int argc, char **argv)
+{
+
+    if (argc != 2)
+    {
+        return false;
+    }
+
+    size_t len = strlen(argv[1]);
+
+    if (len != 26)
+    {
+        return false;
+    }
+
+    if (strspn("ABCDEFGHIJKLMNOPQRSTUVWXYZ", argv[1]) == len || strspn("abcdefghijklmnopqrstuvwxyz", argv[1]) == len)
+    {
+        return true;
+    }
+
+    return false;
+}
