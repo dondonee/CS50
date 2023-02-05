8d11784a9b8950de367fb6ef19ccc1fdac439f40
dondonee <dyn.hwang@gmail.com>
2023-02-05 20:26:58 +0900

    feat: [Runoff] find_min 함수 보충

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -201,8 +201,16 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min = voter_count;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes < min)
+        {
+            min = candidates[i].votes;
+        }
+    }
+
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
