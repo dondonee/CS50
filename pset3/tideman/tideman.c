fcf83a4c9cdc3f783f6bfcedc5f87f3942e0f16b
dondonee <dyn.hwang@gmail.com>
2023-02-11 12:50:52 +0900

    refactor: [Tideman] add_pairs() 조건문 수정

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -132,20 +132,15 @@ void add_pairs(void)
 {
     for (int i = 0; i < candidate_count - 1; i++)
     {
-
         for (int j = i + 1; j < candidate_count; j++)
         {
-            if (preferences[i][j] == preferences[j][i])
-            {
-                continue;
-            }
-            else if (preferences[i][j] > preferences[j][i])
+            if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
                 pair_count++;
             }
-            else
+            else if (preferences[i][j] < preferences[j][i])
             {
                 pairs[pair_count].winner = j;
                 pairs[pair_count].loser = i;
