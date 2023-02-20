66e6df374b8a185d14ade20ddbbecb2033e66d28
dondonee <dyn.hwang@gmail.com>
2023-02-20 10:20:24 +0900

    feat: [Filter] sepia 함수 보충
    - Sepia RGB 값을 계산해주는 get_sepia_color 함수 추가

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,4 +1,7 @@
 #include "helpers.h"
+#include <math.h>
+
+BYTE get_sepia_color(BYTE originalBlue, BYTE originalGreen, BYTE originalRed, char rgb);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -13,14 +16,54 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = average;
         }
     }
-    
+
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE originalBlue = image[i][j].rgbtBlue;
+            BYTE originalGreen = image[i][j].rgbtGreen;
+            BYTE originalRed = image[i][j].rgbtRed;
+
+            image[i][j].rgbtBlue = get_sepia_color(originalBlue, originalGreen, originalRed, 'b');
+            image[i][j].rgbtGreen = get_sepia_color(originalBlue, originalGreen, originalRed, 'g');
+            image[i][j].rgbtRed = get_sepia_color(originalBlue, originalGreen, originalRed, 'r');
+        }
+    }
+
     return;
+}
+
+BYTE get_sepia_color(BYTE originalBlue, BYTE originalGreen, BYTE originalRed, char rgb)
+{
+    double result;
+    switch (rgb)
+    {
+    case 'b':
+        result = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+        break;
+
+    case 'g':
+        result = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+        break;
+
+    case 'r':
+        result = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+        break;
+    }
+
+    if (round(result) > 255)
+    {
+        return (BYTE)255;
+    }
+
+    return (BYTE)result;
 }
 
 // Reflect image horizontally
