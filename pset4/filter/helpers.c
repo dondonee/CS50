99fad3755863a0b2ec0159a3affddb44ed8570ea
dondonee <dyn.hwang@gmail.com>
2023-02-20 17:14:17 +0900

    fix: [Filter] grayscale()에서 소수점 이하가 계산 안되는 버그 수정
    - 계산 변수를 double로 선언하여 소수점 반영하도록 수정
    - RGB 변수에 대입할 때마다 BYTE로 형변환 했던 것을 result 변수에서 한 번만 하도록 수정

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -8,14 +8,19 @@ void blur_pixel(int height, int width, RGBTRIPLE image[height][width], int k, in
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    double rgb_sum;
+    BYTE result;
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            BYTE average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtRed = average;
+            rgb_sum = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
+            result = (BYTE)(round(rgb_sum / 3));
+
+            image[i][j].rgbtBlue = result;
+            image[i][j].rgbtGreen = result;
+            image[i][j].rgbtRed = result;
         }
     }
 
