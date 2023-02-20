7f2976619698c17993911034154e3692a1e731c8
dondonee <dyn.hwang@gmail.com>
2023-02-20 23:04:52 +0900

    feat: [Filter] sobel operator를 이용한 경계선 검출 기능 추가
    - edges() 추가 및 보조 함수들 추가

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,9 +1,14 @@
 #include "helpers.h"
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
 
 BYTE get_sepia_color(BYTE originalBlue, BYTE originalGreen, BYTE originalRed, char rgb);
 void blur_pixel(int height, int width, RGBTRIPLE temp[height][width], RGBTRIPLE image[height][width], int k, int l);
+void edges(int height, int width, RGBTRIPLE image[height][width]);
+int *get_Gx(int height, int width, RGBTRIPLE temp[height][width], int k, int l);
+int *get_Gy(int height, int width, RGBTRIPLE temp[height][width], int k, int l);
+BYTE sobel_opetator(int Gx, int Gy);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -146,4 +151,141 @@ void blur_pixel(int height, int width, RGBTRIPLE temp[height][width], RGBTRIPLE 
     image[k][l].rgbtRed = (BYTE)red;
 
     return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+    memcpy(temp, image, height * width * sizeof(RGBTRIPLE));
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int *Gx = get_Gx(height, width, temp, i, j);
+            int *Gy = get_Gy(height, width, temp, i, j);
+
+            image[i][j].rgbtBlue = sobel_opetator(Gx[0], Gy[0]);
+            image[i][j].rgbtGreen = sobel_opetator(Gx[1], Gy[1]);
+            image[i][j].rgbtRed = sobel_opetator(Gx[2], Gy[2]);
+
+            free(Gx);
+            free(Gy);
+        }
+    }
+
+    return;
+}
+
+int *get_Gx(int height, int width, RGBTRIPLE temp[height][width], int k, int l)
+{
+    int *Gx = malloc(3 * sizeof(int));
+
+    int blue = 0;
+    int green = 0;
+    int red = 0;
+
+    int offset[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int x = 0;
+
+    for (int i = k - 1; i <= k + 1; i++)
+    {
+        int y = 0;
+
+        for (int j = l - 1; j <= l + 1; j++)
+        {
+
+            if (i < 0 || i >= height)
+            {
+                break;
+            }
+
+            if (j < 0 || j >= width)
+            {
+                y++;
+                continue;
+            }
+
+            blue += temp[i][j].rgbtBlue * offset[x][y];
+            green += temp[i][j].rgbtGreen * offset[x][y];
+            red += temp[i][j].rgbtRed * offset[x][y];
+
+            y++;
+        }
+
+        x++;
+    }
+
+    Gx[0] = blue;
+    Gx[1] = green;
+    Gx[2] = red;
+
+    return Gx;
+}
+
+int *get_Gy(int height, int width, RGBTRIPLE temp[height][width], int k, int l)
+{
+    int *Gy = malloc(3 * sizeof(int));
+
+    int blue = 0;
+    int green = 0;
+    int red = 0;
+
+    int offset[3][3] = {{-1, 2, -1}, {0, 0, 0}, {1, 2, 1}};
+    int x = 0;
+
+    for (int i = k - 1; i <= k + 1; i++)
+    {
+        int y = 0;
+
+        for (int j = l - 1; j <= l + 1; j++)
+        {
+
+            if (i < 0 || i >= height)
+            {
+                break;
+            }
+
+            if (j < 0 || j >= width)
+            {
+                y++;
+                continue;
+            }
+
+            blue += temp[i][j].rgbtBlue * offset[x][y];
+            green += temp[i][j].rgbtGreen * offset[x][y];
+            red += temp[i][j].rgbtRed * offset[x][y];
+
+            y++;
+        }
+
+        x++;
+    }
+
+    blue = round(blue / 9);
+    green = round(green / 9);
+    red = round(red / 9);
+
+    Gy[0] = blue;
+    Gy[1] = green;
+    Gy[2] = red;
+
+    return Gy;
+}
+
+BYTE sobel_opetator(int Gx, int Gy)
+{
+    double result = round(sqrt(Gx * Gx + Gy * Gy));
+
+    if (result > 255)
+    {
+        return (BYTE)255;
+    }
+    else if (result < 0)
+    {
+        return (BYTE)0;
+    }
+
+    return (BYTE)result;
 }
