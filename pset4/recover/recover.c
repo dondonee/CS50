ff7c052c121b74416f223a9646b75f85ceb68eca
dondonee <dyn.hwang@gmail.com>
2023-02-27 16:33:48 +0900

    refactor: [Recover] 코드 리팩토링
    - 정수 리터럴 512를 매크로 상수 BLOCK_SIZE에 정의
    - stat()을 이용해 파일 크기를 계산하는 코드를 fseek()을 이용하도록 변경, <sys/stat.h> 삭제
    - 파일 포인터 변수 outptr을 선언시 NULL로 초기화
    - fileCnt를 1 증가하는 코드를 sprintf()안으로 합침
    - isJPEG()함수의 검사 조건 간소화

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <getopt.h>
-#include <sys/stat.h>
 #include <stdbool.h>
 
 typedef uint8_t BYTE;
+
+#define BLOCK_SIZE 512
 
 bool isJPEG(FILE *file);
 void copy_block(FILE *inptr, FILE *outptr);
@@ -28,29 +29,22 @@ int main(int argc, char *argv[])
     }
 
     // Get file size and calculate the number of blocks.
-    struct stat st;
-    stat(infile, &st);
-    const int N = st.st_size / 512;
+    fseek(inptr, 0, SEEK_END);
+    const int N = ftell(inptr) / BLOCK_SIZE;
 
     // Read each 512-byte block of the input file
     int fileCnt = 0;
     char outfile[8];
-    FILE *outptr;
+    FILE *outptr = NULL;
+
     for (int i = 0; i < N; i++)
     {
-        fseek(inptr, 512 * i, SEEK_SET);
+        fseek(inptr, BLOCK_SIZE * i, SEEK_SET);
 
         // If the current block contains the start of a JPEG file, create a new file to write the recovered JPEG data to.
         if (isJPEG(inptr))
         {
-
-            if (fileCnt > 0)
-            {
-                fclose(outptr);
-            }
-
-            sprintf(outfile, "%03i.jpg", fileCnt);
-            fileCnt++;
+            sprintf(outfile, "%03i.jpg", fileCnt++);
             outptr = fopen(outfile, "a");
         }
 
@@ -69,27 +63,17 @@ bool isJPEG(FILE *file)
     fread(&start_bytes, 1, 4, file);
     fseek(file, -4, SEEK_CUR);
 
-    BYTE signiture[3] = {0xff, 0xd8, 0xff};
-    for (int i = 0; i < 3; i++)
+    if (start_bytes[0] == 0xff && start_bytes[1] == 0xd8 && start_bytes[2] == 0xff && (start_bytes[3] & 0xf0) == 0xe0)
     {
-        if (start_bytes[i] != signiture[i])
-        {
-            return false;
-        }
+        return true;
     }
 
-    if ((start_bytes[3] & 0xf0) != 0xe0)
-    {
-        return false;
-    }
-
-    return true;
+    return false;
 }
 
 void copy_block(FILE *inptr, FILE *outptr)
 {
-    BYTE buffer[512];
-    fread(buffer, 1, 512, inptr);
-
-    fwrite(buffer, 1, 512, outptr);
+    BYTE buffer[BLOCK_SIZE];
+    fread(buffer, 1, BLOCK_SIZE, inptr);
+    fwrite(buffer, 1, BLOCK_SIZE, outptr);
 }
